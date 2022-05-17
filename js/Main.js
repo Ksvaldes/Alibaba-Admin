55a951245aac24bd798c2d993ca38d6a12912e20
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-05-16 23:05:11 -0600

    Se actualiza el código

--- js/Main.js
+++ js/Main.js
@@ -1,19 +1,29 @@
-// LIBRERÍA
 
+/* Librería de los programas */
 import {login} from "./login.js"
 
-function recuperar_dato (e){
-    e.preventDefalut();
 
-    var username = document.querySelection("#username").value;
+/** 
+    RECUPERACIÓN DE DATOS
+    la función @recuperar_datos tiene las sigueintes funciones:
+    - Evitar que la página se recargue con preventDefault
+    - Recuperar el usuario ingresado en el input @username
+    - Recuperar la contraseña del input @password
+    - Ejecutar la función @login agregando como parámetros los datos
+      recuperados.
+    - La función es activada por el boton del formulario @login por
+      medio de un evento de tipo submit.
+*/
+function recuperar_datos(e) {
+    e.preventDefault(); //<-- Evita que el formulario se recargue
+
+    var username = document.querySelector("#username").value;
     var password = document.querySelector("#password").value;
 
-    // LOGIN
+    //Programa login
     login(username,password);
 }
 
-//BOTON
-
-var bt_entrar = document.querySelector("#login");
-bt_entrar.addEventListener('submit', recuperar_datos);
-
+// Acción del boton
+var bt_entrar = document.querySelector("#login");//<-- Instancia del formulario
+bt_entrar.addEventListener('submit',recuperar_datos); //<-- Evento
--- js/login.js
+++ js/login.js
@@ -1,21 +1,42 @@
-//LIBRERÍA DEL USUARIO
+/* Descripción del algoritmo 
+    La funcionalidad del algoritmo es la de evaluar el usuario ingresado 
+    e indicar si se encontró el nombre de usuario o correo electrónico.
 
-import {user} from "./user.js/index.js";
+    PARAMETROS:
+    - data: solicita la base de datos en que se busca la información
+    - user: el usuario que se desea buscar en la base de datos.
 
+    PROCEDIMIENTO:
+    - for: recorre la matriz iniciando desde el 0 al 6
+    - if: evalua el usuario ingresado con el indica [i][1], posición
+      del username en la data.
+    - else if: si el user name no existe se evalua el dato ingresado con el
+      indice [i][5], posición del correo electronico.
+    - else: inda que el dato ingresado no existe en la base de datos.
+*/
 
-//VERIFICACIÓN
+/* Librería de componentes */
+export var section_administrador = document.getElementById("section-administrador");
+export var section_login = document.getElementById("section-login");
 
-export function login (username){
+// Librería de usuario
+import {user} from "./User.js";
+import { adminstrador } from "./administrador.js";
 
-    for(let i =0; i <=6; i++){
-        if (user [i][1] === username){
-            console.log("Acceso con nombre de usuario");
-        }else if(user [i][5] === username){
-            console.log("true email");
+// Función de verificación de credenciales
+export function login(username,password){
+
+    for(let i = 0; i<=7;i++){
+        if(username === user[i][1] || username === user[i][5]){
+            if(password === user[i][2]){
+                console.log("Ahora tienes acceso");
+                adminstrador();
+            }else{
+                console.log('Constraseña incorrecta');
+            }
         }else {
-            console.log("user false")
+            console.log("Usuario incorrecto");
         }
-
     }
 
 }
